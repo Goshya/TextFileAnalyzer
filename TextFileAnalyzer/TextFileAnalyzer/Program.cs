907097b1f5bc7c56395f46cb5a297d7c1f970bf6
Goshya <tormozov.g@mail.ru>
2024-08-08 22:24:51 +0300

    create class

--- TextFileAnalyzer/TextFileAnalyzer/Program.cs
+++ TextFileAnalyzer/TextFileAnalyzer/Program.cs
@@ -10,6 +10,17 @@ namespace TextFileAnalyzer
     {
         static void Main(string[] args)
         {
+            TextFileAnalyzer textFile;
+            if (args.Length > 0)
+            {
+                textFile = new TextFileAnalyzer(args[0]);
+            }
+            else
+            {
+                textFile = new TextFileAnalyzer(System.Console.ReadLine());
+            }
+            System.Console.WriteLine("Number of spaces in file: " + textFile.SpaceNumber);
+            System.Console.WriteLine("Number of letters in file: " + textFile.LettersNumber);   
         }
     }
 }
---
+++ TextFileAnalyzer/TextFileAnalyzer/TextFileAnalyzer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TextFileAnalyzer
+{
+    public class TextFileAnalyzer
+    {
+        TextReader tr;
+        private string _filePath;
+        public int SpaceNumber { get; set; } = 0;
+        public int LettersNumber { get; set; } = 0;
+        public TextFileAnalyzer(string filePath)
+        {
+            _filePath = filePath;
+            calcilateLetter();
+            calculateSpaces();
+        }
+
+        public void calculateSpaces()
+        {
+            tr = File.OpenText(_filePath);
+            int i = 0;
+            while ((i = tr.Read()) != -1)
+            {
+                if (i == ' ')
+                {
+                    SpaceNumber++;
+                }
+            }
+            tr.Close();
+        }
+
+        public void calcilateLetter()
+        {
+            tr = File.OpenText(_filePath);
+            int i = 0;
+            while((i = tr.Read()) != -1)
+            {
+                if ((i >= 97 && i <= 122) || (i >= 65 && i <= 90))
+                {
+                    LettersNumber++;
+                }
+            }
+            tr.Close();
+        }
+    }
+}
